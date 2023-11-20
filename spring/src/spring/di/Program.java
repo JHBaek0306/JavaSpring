d33220042b1144876c76cb3b3f82bec34e33182a
JHBaek0306 <cjddns402@naver.com>
2023-11-20 23:17:32 +0900

    temporarily

--- spring/src/spring/di/Program.java
+++ spring/src/spring/di/Program.java
@@ -10,9 +10,17 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		/*
 		Exam exam = new NewlecExam();
-		ExamConsole console = new InlineExamConsole(exam);
-//		ExamConsole console = new GridExamConsole(exam);
+//		ExamConsole console = new InlineExamConsole(exam);
+		ExamConsole console = new GridExamConsole();
+		
+		console.setExam(exam);
+		*/
+		
+		
+		
+		ExamConsole console = 
 		console.print();
 	}
 

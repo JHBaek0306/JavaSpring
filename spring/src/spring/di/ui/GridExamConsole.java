1732b68819b33fe5d479bec2b6791da699c14316
JHBaek0306 <cjddns402@naver.com>
2023-11-26 01:25:07 +0900

    autowired

--- spring/src/spring/di/ui/GridExamConsole.java
+++ spring/src/spring/di/ui/GridExamConsole.java
@@ -1,6 +1,7 @@
 package spring.di.ui;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import spring.di.entity.Exam;
 
@@ -26,6 +27,7 @@ public class GridExamConsole implements ExamConsole {
 	}
 	
 	@Autowired
+	@Qualifier("exam2")
 	@Override
 	public void setExam(Exam exam) {
 		this.exam = exam;

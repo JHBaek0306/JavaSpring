1ea8734c0151ee5a1a6e84f21e8da552fede1fd6
JHBaek0306 <cjddns402@naver.com>
2023-11-27 23:40:01 +0900

    Error

--- spring/src/spring/di/ui/GridExamConsole.java
+++ spring/src/spring/di/ui/GridExamConsole.java
@@ -1,41 +1,27 @@
 package spring.di.ui;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-
 import spring.di.entity.Exam;
 
 public class GridExamConsole implements ExamConsole {
-	
-	@Autowired(required = false)
-	@Qualifier("exam2")
-	private Exam exam;
+
+	private Exam exam;		
 	
 	public GridExamConsole() {
-		System.out.println("construcator");
+		// TODO Auto-generated constructor stub
 	}
-
-	public GridExamConsole(	Exam exam) {
-		System.out.println("overload");
+	public GridExamConsole(Exam exam) {		
 		this.exam = exam;
 	}
-
 	@Override
 	public void print() {
-		System.out.println("-------------------");
-		System.out.println("total     avg");
-		System.out.println("-------------------");
-		if(exam == null)
-			System.out.printf("%3d      %3.2f\n", 0 ,0);
-		else
-			System.out.printf("%3d      %3.2f\n", exam.total(), exam.avg());
-		System.out.println("-------------------");
+		System.out.println("忙式式式式式式式式式式成式式式式式式式式式式忖");
+		System.out.println("弛   total  弛    avg   弛");
+		System.out.println("戍式式式式式式式式式式托式式式式式式式式式式扣");
+		 System.out.printf("弛   %3d  弛    %3.2f   弛\n", exam.total(), exam.avg());
+		System.out.println("戌式式式式式式式式式式扛式式式式式式式式式式戎");
 	}
-	
-
 	@Override
 	public void setExam(Exam exam) {
-		System.out.println("setter");
 		this.exam = exam;
 	}
 
